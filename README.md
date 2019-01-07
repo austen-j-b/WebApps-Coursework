ed6ccfe6cfa55d94be7c78de158c7bc460d85bc1
Austen Brown <austenjbrown@gmail.com>
2019-01-07 13:49:10 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+CSC348 Web Applications Coursework
+
+A web application which allows Admins to create posts which Customers can comment on.
---
+++ WebApps/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WebApps.Data;
+
+[assembly: HostingStartup(typeof(WebApps.Areas.Identity.IdentityHostingStartup))]
+namespace WebApps.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
